c5de4e1bfc327725fff2b3eaf40124db57b58821
perez6736 <perez6736@gmail.com>
2020-09-29 01:16:58 +0900

    its different now

--- tictactoe.py
+++ tictactoe.py
@@ -1,33 +1,40 @@
 import random
 
 def changeSpot(game,shape,spot): ## fix this.
+    x = spot[0]
+    y = spot[1]
     print("change spot - " + shape)
     game[y-1][x-1] = shape 
     showBoard(game)
 
-def selectWhoGoesFirst(playerArray):
+def selectWhoGoesFirst(player):
     n = random.randint(1,2)
     if n == 1:
-        playerArray[0]  = "X"
-        playerArray[1]  = "O"
+        player  = "X"
     else:
-        playerArray[0]  = "O"
-        playerArray[1]  = "X"
-    return playerArray
+        player  = "O"
+    return player
 
 def showBoard(game):
     for row in game:
         print(row)
 
-def checkWin(game):
+def checkWin():
     print("hello")
     return True
+
+def checkTie():
+    return True
+
+def checkIfGameisOver():
+    checkWin()
+    checkTie()
 
 def computersTurn(): ## this will be a min/max algorithm - basically you will never win.
         ## min max. 
     print("min max")
 
-def askPlayerWhere(ply1):
+def askPlayerWhere():
     x = input("select the x coordinate -")
     y = input("select the y coordinate -")
     x = int(x)
@@ -36,7 +43,7 @@ def askPlayerWhere(ply1):
 
     if(x < 1 or x > 3 or y < 1 or y > 3):
         print ("invalid location.")
-        askPlayerWhere(ply1)
+        askPlayerWhere()
     else:
         print(x)
         print(y)
@@ -46,15 +53,17 @@ def askPlayerWhere(ply1):
     
 def playGame():
     ## set game variables
-    game = [[0,0,0],
-            [0,0,0],
-            [0,0,0]]
-    playerArray = ["na", "na"] ##first element is player one and 2 is player 2. 
-    playerArray = selectWhoGoesFirst(playerArray)
-    Player1 = playerArray[0]
-    Player2 = playerArray[1]
+    game = [["-","-","-"],
+            ["-","-","-"],
+            ["-","-","-"]]
+    
+    player = "NA"
+    player2 = "NA"
+    computer ="NA"
 
     showBoard(game)
+
+    
     while(True):## while the game is true - 
         ## who goes first? O goes first. 
         ## ask O to go first 
@@ -63,5 +72,6 @@ def playGame():
         ## computers turn goes. 
         ## check win 
         ## ---- if there is a winner or no more spots - end game. 
+        return
 
 playGame()
